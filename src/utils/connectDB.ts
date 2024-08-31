c3c94bac80f0acdeb126c2071a7620953200e69c
Abdullah Ayoola <officialayo540@gmail.com>
2024-08-31 23:28:34 +0100

    connected to live db

--- src/utils/connectDB.ts
+++ src/utils/connectDB.ts
@@ -3,8 +3,8 @@ const { DB_USER, DB_PASS, DB_NAME } = process.env;
 import mongoose from 'mongoose';
 
 export const connectDB = async () => {
-  // const dbURI = `mongodb+srv://${DB_USER}:${DB_PASS}@quickmnemocluster.vl0va.mongodb.net/${DB_NAME}`;
-  const dbURI = `mongodb://localhost:27017/${DB_NAME}`;
+  const dbURI = `mongodb+srv://${DB_USER}:${DB_PASS}@quickmnemocluster.vl0va.mongodb.net/${DB_NAME}`;
+  // const dbURI = `mongodb://localhost:27017/${DB_NAME}`;
 
   // new comment here
   try {
